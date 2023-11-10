5c1fa8836ed659017b24cc082be52d77ebdc8de3
sirkian <anssisirkia@gmail.com>
2023-11-10 15:26:19 +0200

    More styling for featured PortfolioItem

--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -1,40 +1,49 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { buttonVariants } from "@/components/ui/button";
 
 const PortfolioItem = () => {
+  const liveProject = true;
+
   return (
-    <div className="relative group flex justify-center items-center bg-muted text-primary-foreground h-64 w-80 rounded-lg md:h-80 md:w-[30rem]">
+    <div className="relative group flex justify-center items-center bg-muted dark:bg-background-200 text-primary-foreground h-64 w-96 rounded-lg md:h-80 md:w-[30rem] xl:w-[32rem]">
       <Image
         src="/images/faux1.png"
         alt=""
         fill
-        objectFit="cover"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         className="rounded-md transition-transform transform group-hover:scale-50"
       />
 
-      <div className="absolute group inset-0 flex flex-col justify-between ">
-        <div className="p-2 md:p-4 hidden group-hover:block">
+      <div className="absolute inset-0 flex flex-col justify-between overflow-hidden rounded-lg dark:group-hover:border-2 border-slate-700">
+        <div className="p-2 md:p-4 transition-transform transform translate-y-[-100px] group-hover:translate-y-0 dark:text-primary">
           <h3 className="text-2xl text-center font-extrabold">Project Title</h3>
           <div className="flex gap-3 text-sm justify-center">
             <span>HTML</span>
             <span>CSS</span>
           </div>
         </div>
-        <div className="bg-accent hidden group-hover:block rounded-md min-h-min py-4 px-5 lg:py-5">
+        <div className="bg-accent rounded-md min-h-min py-3 px-5 transition-transform transform translate-y-[140px] group-hover:translate-y-0">
           <p className="text-sm md:text-md mb-4 text-center">
-            Title desc, maybe a few lines worth of explanation. Maybe like this?{" "}
-            <span className="font-semibold border-b-2 border-b-secondary">
-              <Link href="/project/1">See more..</Link>
-            </span>
+            Title desc, maybe a few lines worth of explanation. Maybe like this?
+            <Link
+              className={buttonVariants({ variant: "link" })}
+              href="/project/1">
+              See more..
+            </Link>
           </p>
           <div className="flex justify-between">
-            <span className="bg-background text-sm text-secondary-foreground px-3 py-1.5 rounded-sm">
-              <Link href="">GitHub</Link>
-            </span>
-            <span className="bg-background text-sm text-secondary-foreground px-3 py-1.5 rounded-sm">
-              <Link href="">Live Project</Link>
-            </span>
+            <Link className={buttonVariants({ variant: "secondary" })} href="">
+              GitHub
+            </Link>
+            {liveProject && (
+              <Link
+                className={buttonVariants({ variant: "secondary" })}
+                href="">
+                Live Project
+              </Link>
+            )}
           </div>
         </div>
       </div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "text-text underline-offset-4 hover:underline px-1.5",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -31,7 +31,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
