1c4f9eb47da04695c994c506f842d23b6716bb4a
sirkian <anssisirkia@gmail.com>
2023-11-14 10:29:41 +0200

    Crude layout for project page

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -12,9 +12,47 @@ const SingleProjectPage = () => {
   const project = projects.filter((p) => p.id === id)[0];
 
   return (
-    <main className="min-h-[calc(100vh-7rem)] flex justify-center items-center">
-      <div>{project.title}</div>
-      <ProjectSlider images={project.images} />
+    <main className="min-h-screen flex flex-col items-center">
+      <div className="bg-background-200 w-full xl:w-[90%] 2xl:w-[70%] min-h-[calc(100vh-5rem)] md:mt-[5rem] flex flex-col items-center">
+        <div className="mt-12 p-4">
+          <h1 className="mb-4 font-bold text-2xl lg:text-4xl text-center text-accent">
+            {project.title}
+          </h1>
+          <ProjectSlider images={project.images} />
+        </div>
+        <div className="flex flex-col md:flex-row w-full xl:w-[90%] 2xl:w-[70%] text-center gap-3 md:py-3">
+          <div className="md:w-1/3 p-5 md:order-first">
+            <ul className="my-1">
+              <li>Java</li>
+              <li>JavaScript</li>
+              <li>REST</li>
+              <li>MySQL</li>
+              <li>React.js</li>
+              <li>Deployed on Azure</li>
+            </ul>
+          </div>
+          <div className="md:w-2/3 p-5 md:pr-8 2xl:pr-2 order-first text-justify">
+            <p className="my-1">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod
+              accusamus nobis tempore debitis aperiam. Laudantium velit
+              consectetur ab ratione odit quae asperiores explicabo expedita
+              tempora blanditiis, voluptatum officia fugiat!
+            </p>
+            <p className="my-1">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod
+              accusamus nobis tempore debitis aperiam. Laudantium velit
+              consectetur ab ratione odit quae asperiores explicabo expedita
+              tempora blanditiis, voluptatum officia fugiat!
+            </p>
+            <p className="my-1">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod
+              accusamus nobis tempore debitis aperiam. Laudantium velit
+              consectetur ab ratione odit quae asperiores explicabo expedita
+              tempora blanditiis, voluptatum officia fugiat!
+            </p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
--- src/components/ProjectSlider.tsx
+++ src/components/ProjectSlider.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { AspectRatio } from "./ui/aspect-ratio";
+import { Button } from "./ui/button";
 
 type ImageUrlProps = {
   images: { filename: string; caption: string }[];
@@ -13,18 +15,51 @@ const ProjectSlider: React.FC<ImageUrlProps> = ({ images }) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide((prev) => (prev === images.length - 1 ? 0 : prev + 1));
-    }, 4000);
+    }, 10000);
     return () => clearInterval(interval);
-  }, []);
+  }, [images.length]);
+
+  const handleNextSlide = () => {
+    setCurrentSlide((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  const handlePrevSlide = () => {
+    setCurrentSlide((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
 
   return (
-    <div className="w-[50rem] h-[50rem] relative">
-      <Image
-        src={`/images/${images[currentSlide].filename}`}
-        alt=""
-        fill
-        className="object-cover"
-      />
+    <div className="w-[90vw] xl:w-[70rem] relative">
+      <AspectRatio ratio={16 / 8}>
+        <Image
+          src={`/images/${images[currentSlide].filename}`}
+          alt=""
+          fill
+          className="object-contain"
+        />
+      </AspectRatio>
+
+      <div className="absolute inset-0 flex items-center justify-between">
+        <Button
+          variant="outline"
+          onClick={handlePrevSlide}
+          className="text-xs py-1 px-2">
+          {"<"}
+        </Button>
+        <div className="flex flex-col h-full justify-between items-center w-full">
+          <div className="text-white bg-black bg-opacity-50  w-full text-center p-1 rounded-md">
+            {currentSlide + 1} / {images.length}
+          </div>
+        </div>
+        <Button
+          variant="outline"
+          onClick={handleNextSlide}
+          className="text-xs py-1 px-2">
+          {">"}
+        </Button>
+      </div>
+      <div className="w-full text-center p-1 text-xs md:text-sm text-muted">
+        {images[currentSlide].caption}
+      </div>
     </div>
   );
 };
